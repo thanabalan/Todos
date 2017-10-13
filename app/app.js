9322fbb4bd131e8ba1cb7b1ea18908ec5ef0a171
thanabalan <b.thanabalan@gmail.com>
2017-10-14 02:36:18 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var appModule = angular.module("myApp", ["ngRoute", "LocalStorageModule"]);
 appModule.config(function ($routeProvider) {
     $routeProvider
     .when("/", {
-        templateUrl: "/Users.html"
+        templateUrl: "../Users.html"
     })
     .when("/Todos", {
         templateUrl: "/Todos.html",
