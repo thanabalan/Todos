15601298df8c441a68d7506afc6b9a09b92d5533
thanabalan <b.thanabalan@gmail.com>
2017-10-14 03:08:23 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var appModule = angular.module("myApp", ["ngRoute", "LocalStorageModule"]);
 appModule.config(function ($routeProvider) {
     $routeProvider
     .when("/", {
-        template: '<div class="content" ng-controller="UserController"> <h3 class="page-header"><b>User Cards</b></h3> <div class="row" ><div ng-repeat="User in users">         <div ng-class="{ 'row': ($index + 1) % 4 == 0 }"> <div class="col-sm-3"> <h3><strong>{{ User.name }} </strong></h3>    <p><b>Email</b> : {{  User.email }}</p>                        <p><b>Phone</b> : {{  User.phone }}</p>   <p><b>website</b> : {{  User.website }}</p>  <p><b>Company Name</b> : {{  User.company.name }}</p>                        <div class="col-sm-12 text-right">   <a ng-click="TodoClick(User.id)" style="cursor:pointer; font-weight:bold">View Todos</a>  </div> </div>    </div> </div>   </div> </div></div>'    ,
+        template: "https://thanabalan.github.io/Todos/Users.html" ,
     })
     .when("/Todos", {
         templateUrl: "https://thanabalan.github.io/Todos/Todos.html",
